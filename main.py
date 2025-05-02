792092e6dfceb44a6e41201d93e9434e0d85ee89
edwellshtc <edwellshtc@gmail.com>
2025-05-02 19:26:56 +0100

    F: Added player and enemy images with animation

--- main.py
+++ main.py
@@ -11,7 +11,20 @@ def main():
     screen = pygame.display.set_mode((1200, 720)) #Screen window size
     pygame.display.set_caption("Battler") #Naming the window
     clock = pygame.time.Clock()
+
     background = pygame.image.load('graphics/Battleground2.png')
+    background = pygame.transform.scale(background, (1200, 720))
+
+    player_surface = pygame.image.load('graphics/Knight_idle.png')
+    player_surface = pygame.transform.scale(player_surface, (500, 500)) #Resizing player image
+    player_x_pos = -200
+
+    #enemy icon, change this to take various enemies at later date?
+    enemy_surface = pygame.image.load('graphics/skel_at_1.png')
+    enemy_surface = pygame.transform.scale(enemy_surface, (500, 500))
+    enemy_surface = pygame.transform.flip(enemy_surface, True, False) #Flip the enemies icon
+    enemy_x_pos = 900
+
 
     while True:
 
@@ -19,7 +32,17 @@ def main():
             if event.type == pygame.QUIT:
                 exit()
 
-        screen.blit(background,(0,0)) #Display base surface - the dungeon background
+        screen.blit(background,(0,0))#Display base surface - the dungeon background
+
+        #Create player and move them from left at start
+        screen.blit(player_surface,(player_x_pos, 0))
+        if player_x_pos < 200:
+            player_x_pos += 2
+
+        #Create enemy and move them from right at start
+        screen.blit(enemy_surface,(enemy_x_pos, 0))
+        if enemy_x_pos > 500:
+            enemy_x_pos -=2
 
         pygame.display.update()
         clock.tick(60) #making the loop run 60 times per second (or frames)
